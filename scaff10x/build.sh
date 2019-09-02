cfc7a290aee5a21d91111e65b7135b0241c27953
Dan Sun <dan.sun@agresearch.co.nz>
2019-09-02 15:25:53 +1200

    Finalised scaff10x's initial build

--- scaff10x/build.sh
+++ scaff10x/build.sh
@@ -1,9 +1,27 @@
+# Compile Scaff10X
 cd src
 make CC=$CC AR=$AR CFLAGS="-O2 -std=c11 -march=x86-64 -mtune=generic -I${BUILD_PREFIX}/include" LFLAGS="-L${BUILD_PREFIX}/lib -lm -pthread -lz"
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/etc
-cp break10x $PREFIX/bin/
-cp scaff10x $PREFIX/bin/
-cp scaff_reads $PREFIX/bin/
-cp -r scaff-bin/ $PREFIX/bin/
+# Copy Scaff10X's binaries to a sub directory in $PREFIX/bin
+mkdir -p $PREFIX/bin/Scaff10X
+
+cp break10x $PREFIX/bin/Scaff10X/
+cp scaff10x $PREFIX/bin/Scaff10X/
+cp scaff_reads $PREFIX/bin/Scaff10X/
+cp -r scaff-bin/ $PREFIX/bin/Scaff10X/
+
+# Create custom lanuch script, so scaff10x and break10x do not be called bia its full path
+cat > $PREFIX/bin/scaff10x<< "EOF"
+#!/bin/bash
+DIRECTORY=$(cd `dirname $0` && pwd)
+$DIRECTORY/Scaff10X/scaff10x "$@"
+EOF
+
+cat > $PREFIX/bin/break10x<< "EOF2"
+#!/bin/bash
+DIRECTORY=$(cd `dirname $0` && pwd)
+$DIRECTORY/Scaff10X/break10x "$@"
+EOF2
+
+chmod +x $PREFIX/bin/scaff10x
+chmod +x $PREFIX/bin/break10x
---
+++ scaff10x/post-link.sh
@@ -0,0 +1,4 @@
+# Created SymLinks for Scaff10X acessing its dependencies deployed as runtim dependencies
+ln -s ../../bwa $PREFIX/bin/Scaff10X/scaff-bin/bwa
+ln -s ../../pigz $PREFIX/bin/Scaff10X/scaff-bin/pigz
+ln -s ../../smalt $PREFIX/bin/Scaff10X/scaff-bin/smalt
