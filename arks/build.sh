2fab32a8c32b8a44f421d897efa0066f9220d509
Dan Sun <dan.sun@agresearch.co.nz>
2019-09-12 17:13:09 +1200

    Added correct runtime dependency and a wrapper

--- arks/build.sh
+++ arks/build.sh
@@ -1,6 +1,11 @@
 ./autogen.sh
-#./configure --with-boost=${CONDA_PREFIX}/include --prefix=${CONDA_PREFIX} CXXFLAGS="-Wall -Wextra -Werror -std=c++0x -Wno-unused-result" LDFLAGS="-L$CONDA_PREFIX/lib"
 ./configure --with-boost=${BUILD_PREFIX}/include --prefix=${PREFIX} CXXFLAGS="-Wall -Wextra -Werror -std=c++0x -Wno-unused-result" LDFLAGS="-L$BUILD_PREFIX/lib"
 make install AR=$AR
 
 cp -r ./Examples $PREFIX/
+
+cat > $PREFIX/bin/arks-make<< "EOF"
+#!/bin/bash
+DIRECTORY=$(cd `dirname $0` && pwd)
+make -f $DIRECTORY/../Examples/arks-make "$@"
+EOF
